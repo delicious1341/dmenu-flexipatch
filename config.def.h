9ec15f31cfaea0a6179e9b097b5c6db2f413a5bc
delicious1341 <130315844+delicious1341@users.noreply.github.com>
2023-04-28 23:11:07 +0000

    Update config.def.h

--- config.def.h
+++ config.def.h
@@ -35,7 +35,7 @@ static char *fonts[] =
 static const char *fonts[] =
 #endif // XRESOURCES_PATCH
 {
-	"monospace:size=10"
+	"JetBrainsMono Nerd Font Mono:style=Regular:pixelsize=20"
 };
 #endif // PANGO_PATCH
 #if MANAGED_PATCH
